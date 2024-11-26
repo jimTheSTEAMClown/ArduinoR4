5129679fc285fe0901a0b337c5e8d3c17672986c
Jim Burnham <jim.the.steam.clown@gmail.com>
2024-11-26 12:08:59 -0800

    Create R4-WiFi-Blink.ino

---
+++ R4-WiFi-Blink.ino
@@ -0,0 +1,94 @@
+//#include "R4WiFi_secrets.h"
+#define SECRET_SSID         "Your_SSID";
+#define SECRET_PASS         "Your SSID-Password";
+#include <R4HttpClient.h>
+
+
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
+*/
+
+// the setup function runs once when you press reset or power the board
+WiFiSSLClient client;
+R4HttpClient http;
+
+const char* _SSID = SECRET_SSID;
+const char* _PASS = SECRET_PASS;
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial);
+
+  String fv = WiFi.firmwareVersion();
+  Serial.println(fv);
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION)
+    Serial.println(F("Please upgrade the firmware"));
+    Serial.println(WIFI_FIRMWARE_LATEST_VERSION);
+  if (WiFi.status() == WL_NO_MODULE)
+  {
+    Serial.println(F("Communication with WiFi module failed!"));
+    while (true);
+  }
+
+  WiFi.begin(_SSID, _PASS);
+  Serial.print(F("Connecting to WiFi"));
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print(F("."));
+  }
+  Serial.println();
+  Serial.println(F("Successfully connected to WiFi!"));
+
+  //http.begin(client, "https://www.steamclown.org/projects/red_pill.html", 443);  
+  http.begin(client, "https://icanhazdadjoke.com/slack", 443);
+  http.setTimeout(5000); //3000
+  http.addHeader("User-Agent: Arduino UNO R4 WiFi");
+  http.addHeader("Connection: close");
+
+  int responseNum = http.GET();
+  Serial.println(responseNum);
+  //responseNum = 200;
+  //if (responseNum > 0) // OR 
+  if (responseNum == HTTP_CODE_OK) // 200 OK
+  {
+    // Get body
+    String responseBody = http.getBody();
+    Serial.println(responseBody);
+    Serial.println("Response code: " + String(responseNum));
+  } else {
+    Serial.println("Request Failed: " + String(responseNum));
+  }
+
+  http.close();
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(200);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(200); 
+}
+
